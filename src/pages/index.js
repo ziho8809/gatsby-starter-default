344eeea4a76507c76855d36383abeab049e12710
ziho8809 <ziho8809@naver.com>
2022-04-15 18:10:57 +0900

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi peopletttttttttttt</h1>
+    <p>Welcome to your netttttttttttttttw Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
